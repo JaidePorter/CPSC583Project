f0a8e6fcdad877a3c5d3a766da0b3587e6eefe49
Jayden Bischoff <jayden.bischoff1@ucalgary.ca>
2023-04-02 14:49:15 -0600

    Initial development of vis 3

--- src/App.js
+++ src/App.js
@@ -1,41 +1,11 @@
-import logo from './logo.svg';
 import './App.css';
-import ReactECharts from 'echarts-for-react';
+import React from "react";
+import Visualization3 from './Visualization3';
 
 function App() {
-  const option = {
-    xAxis: {
-      type: 'category',
-      data: ['Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat', 'Sun']
-    },
-    yAxis: {
-      type: 'value'
-    },
-    series: [
-      {
-        data: [120, 200, 150, 80, 70, 110, 130],
-        type: 'bar'
-      }
-    ]
-  }; 
-
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-      <ReactECharts option={option} />
+      <Visualization3/>
     </div>
   );
 }
