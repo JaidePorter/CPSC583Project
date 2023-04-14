800d871972840282b2324d88bfa2ec9b85975d67
Bryce <brycepcayanan@gmail.com>
2023-04-14 13:55:21 -0600

    Commit

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import './App.css';
 import React from "react";
+import Homepage from './Homepage';
 import Visualization1 from './Visualization1';
 import Visualization2 from './Visualization2';
 import Visualization3 from './Visualization3';
@@ -7,6 +8,7 @@ import Visualization3 from './Visualization3';
 function App() {
   return (
     <div className="App">
+      <Homepage/>
       <Visualization1/>
       <Visualization2/>
       <Visualization3/>
---
+++ src/Homepage.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import "./Homepage.css"
+
+function Homepage() {
+
+  return (
+    <div style={{height: "100vh", width: "100%", backgroundColor: "blue"}}>
+
+    </div>
+  );
+}
+
+export default Homepage;
+  
