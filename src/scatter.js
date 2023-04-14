0508ea60b248e1a740d3ce9f53e909ad0a61981e
faiiliaqat <79996433+faiiliaqat@users.noreply.github.com>
2023-04-14 11:17:27 -0600

    Add files via upload
    added a zoom-in feature, fixed the scale, and limited genre selection to 5.

--- src/scatter.js
+++ src/scatter.js
@@ -3,8 +3,10 @@ import * as echarts from 'echarts';
 import data from './data.json';
 import "./scatter.css"
 
+
 function ScatterPlot() {
 
+    const MAX_CHECKED_GENRES = 5;
     const [selectedFeature, setSelectedFeature] = useState("valence"); // set your initial feature here
     const [selectedGenres, setSelectedGenres] = useState({
           "Children's Music": false,
@@ -35,8 +37,9 @@ function ScatterPlot() {
           "A Capella": false,
        
       });
- 
-    const chartRef = useRef(0); // 
+
+  
+    const chartRef = useRef(null); // 
 
     const genreColorMap = {
         "Children's Music": "#e41a1c",
@@ -44,50 +47,38 @@ function ScatterPlot() {
         "Comedy": "#4daf4a",
         "Folk": "#984ea3",
         "Electronic": "#ff7f00",
-        "Indie": "#ffff29",
-        "Hip-Hop": "#a65628",
-        "Pop": "#f781bf",
-        "Rock": "#999999",
-        "Rap": "#dede00",
-        "Jazz": "#d0d0d0",
-        "Soul": "#696969",
-        "Classical": "#ff7f50",
-        "World": "#b0e0e6",
-        "Alternative": "#8b4513",
-        "R&B": "#00008b",
-        "Blues": "#e6beff",
-        "Anime": "#808000",
-        "Reggae": "#ffd700",
-        "Reggaeton": "#7f007f",
-        "Ska": "#000000",
-        "Dance": "#6b8e23",
-        "Country": "#ffa07a",
-        "Opera": "#ffb6c1",
-        "Movie": "#2f4f4f",
-        "A Capella": "#c71585"
+        "Indie": "#ffff29",   "Hip-Hop": "#a65628", "Pop": "#f781bf", "Rock": "#beaed4",  "Rap": "#dede00",  "Jazz": "#d0d0d0",
+        "Soul": "#696969", "Classical": "#ff7f50", "World": "#b0e0e6", "Alternative": "#8b4513", "R&B": "#999999", "Blues": "#e6beff",
+        "Anime": "#808000", "Reggae": "#ffd700", "Reggaeton": "#", "Ska": "#F64C72", "Dance": "#6b8e23", "Country": "#ffa07a",
+        "Opera": "#ffb6c1", "Movie": "#2f4f4f", "A Capella": "#c71585"
       }; 
+
+
   
     useEffect(() => {
-      const chart = echarts.init(chartRef.current);
 
-      // featurs array
-      let trackNames = data.map((d) => d.track_name);
-      let popularity = data.map((d) => d.popularity);
-      let valence = data.map((d) => d.valence);
-      let acousticness = data.map((d) => d.acousticness);
-      let danceability = data.map((d) => d.danceability);
-      let energy = data.map((d) => d.energy);
-      let instrumentalness = data.map((d) => d.instrumentalness);
-      let liveness = data.map((d) => d.liveness);
-      let tempo = data.map((d) => d.tempo);
-      let loudness = data.map((d) => d.loudness);
-      let speechiness = data.map((d) => d.speechiness);
-      // let duration_ms_normalized = data.map((d) => d.duration_ms_normalized);
-      let genre = data.map((d) => d.genre);
-      // const filteredData = data.filter(d => selectedGenres.includes(genre));
-      // Create array for selected feature
+    
+    const chart = echarts.init(chartRef.current);
 
-      let seriesData = [];
+    // featurs array
+    let trackNames = data.map((d) => d.track_name);
+    let popularity = data.map((d) => d.popularity);
+    let valence = data.map((d) => d.valence);
+    let acousticness = data.map((d) => d.acousticness);
+    let danceability = data.map((d) => d.danceability);
+    let energy = data.map((d) => d.energy);
+    let instrumentalness = data.map((d) => d.instrumentalness);
+    let liveness = data.map((d) => d.liveness);
+    let tempo = data.map((d) => d.tempo);
+    let loudness = data.map((d) => d.loudness);
+    let speechiness = data.map((d) => d.speechiness);
+    
+    let genre = data.map((d) => d.genre);
+    // const filteredData = data.filter(d => Object.values(selectedGenres).includes(d.genre));
+  
+
+      // let seriesData = [];
+      
 
       const features = {
         valence: valence,
@@ -100,29 +91,18 @@ function ScatterPlot() {
         loudness: loudness,
         speechiness: speechiness,
         genre: genre,
+        
       };
 
       const option = {
-        // legend: {
-        //   show: true,
-        //   right: 5,
-        //   top: 20,
-        //   bottom: 20,
-     
-        //   // data: seriesData.map(category => category.genre),
-        //   textStyle: {
-        //     color: '#333'
-        //   },
-          // itemGap: 20
-        // },
+
         xAxis: {
-          type: 'value',
-          name: selectedFeature,
-          nameLocation: 'middle',
-          nameGap: 30, // adjust this value as needed to center the label
-          nameTextStyle: {
-            fontSize: 30
-          },
+          type: 'value'
+        //   name: selectedFeature,
+        //   nameLocation: 'middle',
+        //   nameTextStyle: {
+        //     fontSize: 18
+        //   },
         },
         yAxis: {
           type: 'value',
@@ -133,15 +113,16 @@ function ScatterPlot() {
             fontSize: 30
           }
         },
+
         series: [
           {
             type: 'scatter',
             data: (() => {
-                // let seriesData = [];
+                let seriesData = [];
                 const xData = features[selectedFeature]; // get x data based on selected feature
                 for (let i = 0; i < xData.length; i++) {
 
-                  if (selectedGenres[genre[i]]) { // check if genre is selected
+                  if (selectedGenres[genre[i]] === true) { // check if genre is selected
                     seriesData.push({
                       value: [xData[i], popularity[i]],
                       name: trackNames[i],
@@ -157,10 +138,26 @@ function ScatterPlot() {
                 }
                 return seriesData;
             })(),
-            // legendHoverLink: true,
-            name: 'genre',
+        
           },  
         ],
+        toolbox: {
+          showTitle: true, // add title to toolbox
+          feature: {
+            dataZoom: {
+              iconSize: [50, 50], // add iconSize to increase the size of the icons  
+              iconStyle: {
+                emphasis:{
+        
+                  shadowOffsetY: -0.5,
+                  borderWidth: 3 ,
+                }
+              }
+            }
+          },
+          right: '10%', // position of toolbox
+          top: 'top'
+        },
 
         tooltip: {
           trigger: 'item',
@@ -178,6 +175,9 @@ function ScatterPlot() {
             fontSize: '16px'
           }
         },
+
+
+        
       };
       chart.setOption(option);
   
@@ -187,35 +187,47 @@ function ScatterPlot() {
       
     }, [selectedFeature, selectedGenres] );
   
-    /*
     return (
       <div>
-        <div style= {{display: 'grid',  gridTemplateColumns: 'auto auto', marginLeft: "1em"}}>
-          <div>
-            <h1>Comparison of Popularity Scores across Different Music Genres</h1>
-            <div ref={chartRef} style={{ width: '100%', height: '100%', backgroundColor: "white" }}></div>
-          </div>
-          <div className="filter-container">   
-            <h3>Select Genres:</h3>
-            {Object.entries(genreColorMap).map(([genre, color]) => (
-              <label key={genre} style={{display: "block", backgroundColor: color, color: color === "#000000" || color === "#00008b" ? "white" : "black", maxWidth: "100%", fontWeight: "bold", padding: "10px", borderRadius: "5px", marginBottom: "5px", display: "flex", alignItems: "center"}}>
-              <input
-                  type="checkbox"
-                  value ={genre}
-                  checked={selectedGenres[genre]}
-                  onChange={() => {
-                    setSelectedGenres({
-                      ...selectedGenres,
-                      [genre]: !selectedGenres[genre]
-                    });
-                  }}
-                  style={{ marginRight: "10px", width: "20px", height: "20px", backgroundColor: color, borderRadius: "5px", border: "none", outline: "none", cursor: "pointer"}}
-              />
-            {genre}
-              </label>
-            ))}
-          </div>
+      <div class="grid-container">
+        <div>
+          <h1>Comparison of Popularity Scores across Different Music Genres</h1>
+          <div ref={chartRef} class= "chart-container">  </div>
         </div>
+        <div className="filter-container">
+             
+             <h3>Select Genres:</h3>
+             {Object.entries(genreColorMap).map(([genre, color]) => (
+                  
+                  <label
+                  key={genre}
+                  style={{
+                    display: "block",
+                    backgroundColor: selectedGenres[genre] ? color : "transparent",
+                    color: color ===  "black",
+
+                  }}
+                >
+                  <input
+                    type="checkbox"
+                    value={genre}
+                    checked={selectedGenres[genre]}
+                    onChange={() => {
+                      const checkedCount = Object.values(selectedGenres).filter(Boolean).length;
+                      if (checkedCount < MAX_CHECKED_GENRES || selectedGenres[genre]) {
+                        setSelectedGenres({
+                          ...selectedGenres,
+                          [genre]: !selectedGenres[genre],
+                        });
+                      }
+                    }}
+
+                  />
+                  {genre}
+                </label>
+                 
+             ))}
+             </div>
 
         <div style={{textAlign: "center"  , marginBottom: '2%', fontSize: '30px' }}>
           Select feature: 
@@ -231,53 +243,15 @@ function ScatterPlot() {
             <option value="speechiness">Speechiness</option>
           </select>
         </div>
-      </div>
-    );
-    */
-    return (
-      <div style={{height: "100vh", width: "100%", display: "grid", gridTemplateColumns: "79% 19%", gap: "2%"}}>
-        <div style={{height: "100%", width: "100%", display: "grid", gridTemplateRows: "fit-content(100%) auto fit-content(100%)", paddingLeft: "1%"}}>
-          <h1>Comparison of Popularity Scores across Different Music Genres</h1>
-          <div ref={chartRef} style={{width: '100%', height: '100%', backgroundColor: "white"}}/>
-          <div style={{textAlign: "center", marginTop: "2rem", marginBottom: "2rem", fontSize: '30px' }}>
-            Select feature: 
-            <select value={selectedFeature} onChange={(e) => setSelectedFeature(e.target.value)} style={{fontSize: "30px" , textAlign: "center", background: "#E8A87C", color: "white", borderRadius: "10px", fontSize: "2rem", padding: "0.25rem"}}>
-              <option value="valence">Valence</option>
-              <option value="acousticness">Acousticness</option>
-              <option value="danceability">Danceability</option>
-              <option value="energy">Energy</option>
-              <option value="instrumentalness">Instrumentalness</option>
-              <option value="liveness">Loudness</option>
-              <option value="tempo">Tempo</option>
-              <option value="loudness">Loudness</option>
-              <option value="speechiness">Speechiness</option>
-            </select>
-          </div>
         </div>
-        <div className="filter-container">
-          <h3>Select Genres:</h3>
-          <div className="filter-sensors-container">
-            {Object.entries(genreColorMap).map(([genre, color]) => (
-              <label key={genre} style={{display: "block", backgroundColor: color, color: color === "#000000" || color === "#00008b" ? "white" : "black", maxWidth: "100%", fontWeight: "bold", padding: "10px", borderRadius: "5px", marginBottom: "5px", marginLeft: "10px", marginRight: "10px", display: "flex", alignItems: "center"}}>
-              <input
-                  type="checkbox"
-                  value ={genre}
-                  checked={selectedGenres[genre]}
-                  onChange={() => {
-                    setSelectedGenres({
-                      ...selectedGenres,
-                      [genre]: !selectedGenres[genre]
-                    });
-                  }}
-                  style={{ marginRight: "10px", width: "20px", height: "20px", backgroundColor: color, borderRadius: "5px", border: "none", outline: "none", cursor: "pointer"}}
-              />
-              {genre}
-              </label>
-            ))}
-          </div>
-          <h3 style={{color: "white"}}>Select Genres:</h3>
-        </div>
+
+       
       </div>
+      
+      
+
+
+
     );
   }
   
