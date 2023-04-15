0eb84d4a77988ee519dc31f414deb7a4f4185853
JaidePorter <44730154+JaidePorter@users.noreply.github.com>
2023-04-15 06:39:21 -0600

    Added filter for genre (#6)
    * Added filter for genre
    
    * Added visualization description
    
    * fixed data bug
    
    * Fix css class name conflicts
    
    ---------
    
    Co-authored-by: Bryce <brycepcayanan@gmail.com>

--- src/Visualization2.js
+++ src/Visualization2.js
@@ -6,6 +6,7 @@ import data from './data.json';
 function Visualization2() {
   
   const [popularity, setPopularity] = useState(100);
+  const [genreSelected, setGenre] = useState("all");
 
   data = data.sort((a, b) => {
     if(a.popularity < b.popularity) {
@@ -22,92 +23,106 @@ function Visualization2() {
   let speechinessData = [];
   let valenceData = [];
   let genreData = {};
-  let genreList = [];
+  let currentLine = "all";
+  let genreCounts = {
+    "all": 50000,
+    "A Capella": 21,
+    "Alternative": 1958,
+    "Anime": 1926,
+    "Blues": 1935,
+    "Children's Music": 3189,
+    "Classical": 1972,
+    "Comedy": 2109,
+    "Country": 1823,
+    "Dance": 1843,
+    "Electronic": 2054,
+    "Folk": 2067,
+    "Hip-Hop": 2022,
+    "Indie": 2046,
+    "Jazz": 1985,
+    "Movie": 1643,
+    "Opera": 1744,
+    "Pop": 2015,
+    "R&B": 1952,
+    "Rap": 1995,
+    "Reggae": 1882,
+    "Reggaeton": 1879,
+    "Rock": 2000,
+    "Ska": 1861,
+    "Soul": 1982,
+    "Soundtrack": 2126, 
+    "World": 1971
+  }
+  let songCount = genreCounts[genreSelected];
+  var a = 0;
+  var b = 0;
 
-  for(var i = 0 ; i < (popularity * 50000 / 100); i++) {
-    let index = 50000 - i - 1;
-    popularityData.push(data[index]["popularity"]);
-    danceabilityData.push(data[index]["danceability"]);
-    acousticnessData.push(data[index]["acousticness"]);
-    energyData.push(data[index]["energy"]);
-    instrumentalnessData.push(data[index]["instrumentalness"]);
-    livenessData.push(data[index]["liveness"]);
-    speechinessData.push(data[index]["speechiness"]);
-    valenceData.push(data[index]["valence"]);
-    let genre = data[index]["genre"];
-    genreList.push(genre);
-    let newEntry = [
-      data[index]["danceability"],
-      data[index]["acousticness"],
-      data[index]["energy"],
-      data[index]["instrumentalness"],
-      data[index]["liveness"], 
-      data[index]["speechiness"], 
-      data[index]["valence"]];
-    if(!genreData.hasOwnProperty(genre)) {
-      genreData[genre] = {
-        "count": 1,
-        "data": newEntry
-      }
+  while(b < (popularity * songCount / 100)) {
+    let index = 50000 - a - 1;
+    if(genreSelected == "all") {
+      popularityData.push(data[index]["popularity"]);
+      danceabilityData.push(data[index]["danceability"]);
+      acousticnessData.push(data[index]["acousticness"]);
+      energyData.push(data[index]["energy"]);
+      instrumentalnessData.push(data[index]["instrumentalness"]);
+      livenessData.push(data[index]["liveness"]);
+      speechinessData.push(data[index]["speechiness"]);
+      valenceData.push(data[index]["valence"]);
+      b++;
     }
     else {
-      genreData[genre].count += 1;
-      for(var j = 0; j < 7; j++) {
-        genreData[genre].data[j] += newEntry[j];
+      let genre = data[index]["genre"];
+      if(genre == genreSelected) {
+        let newEntry = [
+          data[index]["danceability"],
+          data[index]["acousticness"],
+          data[index]["energy"],
+          data[index]["instrumentalness"],
+          data[index]["liveness"], 
+          data[index]["speechiness"], 
+          data[index]["valence"]];
+        if(!genreData.hasOwnProperty(genre)) {
+          genreData[genre] = {
+            "count": 1,
+            "data": newEntry
+          }
+        }
+        else {
+          genreData[genre].count += 1;
+          for(var j = 0; j < 7; j++) {
+            genreData[genre].data[j] += newEntry[j];
+          }
+        }
+        b++;
       }
     }
+    a++;
   }
 
-  let radarData = [];
   for(var i in genreData) {
      for(var j = 0; j < 7; j++) {
       genreData[i].data[j] /= genreData[i].count;
      }
-     radarData.push({
-      "name": genreData[i],
-      "value": genreData[i].data
-     })
   }
-
-  console.log(genreData)
-  const genreColorMap = {
-    "Children's Music": "#e41a1c",
-    "Soundtrack": "#377eb8",
-    "Comedy": "#4daf4a",
-    "Folk": "#984ea3",
-    "Electronic": "#ff7f00",
-    "Indie": "#ffff29",
-    "Hip-Hop": "#a65628",
-    "Pop": "#f781bf",
-    "Rock": "#999999",
-    "Rap": "#dede00",
-    "Jazz": "#d0d0d0",
-    "Soul": "#696969",
-    "Classical": "#ff7f50",
-    "World": "#b0e0e6",
-    "Alternative": "#8b4513",
-    "R&B": "#00008b",
-    "Blues": "#e6beff",
-    "Anime": "#808000",
-    "Reggae": "#ffd700",
-    "Reggaeton": "#7f007f",
-    "Ska": "#000000",
-    "Dance": "#6b8e23",
-    "Country": "#ffa07a",
-    "Opera": "#ffb6c1",
-    "Movie": "#2f4f4f",
-    "A Capella": "#c71585"
-  }; 
 
   function getAvg(data) {
     const total = data.reduce((acc, c) => acc + c, 0);
     return total / data.length;
   }
 
+  let dataDisplayed = [];
+  if(genreSelected == "all") {
+    let allSongs = [getAvg(danceabilityData), getAvg(acousticnessData), getAvg(energyData), getAvg(instrumentalnessData), getAvg(livenessData), getAvg(speechinessData), getAvg(valenceData)];
+    dataDisplayed = allSongs;
+  }
+  else {
+    dataDisplayed = genreData[genreSelected].data;
+  }
+
+  if(dataDisplayed.length == 0) {
+    updateLine(currentLine);
+  }
   const option = {
-    legend: {
-      // data: genreList
-    },
     radar: {
       indicator: [
         { name: "Danceability", max: 1 },
@@ -153,30 +168,88 @@ function Visualization2() {
         type: "radar",
         data: [
           {
-            value: [getAvg(danceabilityData), getAvg(acousticnessData), getAvg(energyData), getAvg(instrumentalnessData), getAvg(livenessData), getAvg(speechinessData), getAvg(valenceData)]
+            value: dataDisplayed
           }
         ],
         lineStyle: {color: '#E27D60', width: 5,},
-        
         itemStyle: {color: '#E27D60'}
-        // data: radarData
       }
     ]
-  }; 
+  };
+
+  function updateLine(e) {
+    setGenre(e);
+    currentLine = e;
+  }
 
   function updateSlider(e) {
     setPopularity(e.target.value)
     let t = document.getElementById("tooltip");
     t.innerHTML = "Top " + e.target.value + "%";
+    updateLine(currentLine);
   }
 
   return (
     <div className="visualization-container">
       <h1>How do musical features positively or negatively impact a song's popularity?</h1>
-      <ReactECharts height="600px" className="vis2canvas" option={option} />
-      <input type="range" id="visualization2slider" min="1" max="100" defaultValue="100" onChange={e => updateSlider(e)} />
-      <output id="tooltip">Top 100%</output>
-      <h3>Popularity</h3>
+      <div className="v2-grid-container">
+        <div>
+          <label>Select Genre: </label>
+          <select onChange={e => updateLine(e.target.value)}>
+            <option value="all">All Songs</option>
+            <option value="A Capella">Acapella</option>
+            <option value="Alternative">Alternative</option>
+            <option value="Anime">Anime</option>
+            <option value="Blues">Blues</option>
+            <option value="Children's Music">Children's Music</option>
+            <option value="Classical">Classical</option>
+            <option value="Comedy">Comedy</option>
+            <option value="Country">Country</option>
+            <option value="Dance">Dance</option>
+            <option value="Electronic">Electronic</option>
+            <option value="Folk">Folk</option>
+            <option value="Hip-Hop">Hip-Hop</option>
+            <option value="Indie">Indie</option>
+            <option value="Jazz">Jazz</option>
+            <option value="Movie">Movie</option>
+            <option value="Opera">Opera</option>
+            <option value="Pop">Pop</option>
+            <option value="R&B">R&B</option>
+            <option value="Rap">Rap</option>
+            <option value="Reggae">Reggae</option>
+            <option value="Reggaeton">Reggaeton</option>
+            <option value="Rock">Rock</option>
+            <option value="Ska">Ska</option>
+            <option value="Soul">Soul</option>
+            <option value="Soundtrack">Soundtrack</option>
+            <option value="World">World</option>
+          </select>
+          <ReactECharts height="600px" className="vis2canvas" option={option} />
+          <input type="range" id="visualization2slider" min="1" max="100" defaultValue="100" onChange={e => updateSlider(e)} />
+          <output id="tooltip">Top 100%</output>
+          <h3>Popularity</h3>
+        </div>
+        <div className="text-container">
+          <p>
+            The radar plot presented here displays the positive and negative impacts of various musical attributes on the popularity of a song. 
+            The slider enables the user to adjust the number of songs included in the visualization. For example, selecting the top 30% would 
+            display the attributes of the 30% most popular songs in the dataset, while selecting the top 100% would show all songs.
+            <br/>
+            <br/>
+            The selection field can be used to filter the data by genre, allowing the user to view the attributes of songs in a specific genre. 
+            By default, the plot shows the attributes of all songs.
+            <br/>
+            <br/>
+            Comparing the default configuration (showing the top 100% of all songs) with the top 30%, we can observe that danceability 
+            and energy have a greater impact on a song's popularity, while acousticness and instrumentalness have a negative impact. 
+            However, these values may vary when the plot is filtered by genre.
+            <br/>
+            <br/>
+            Artists can use this visualization to identify the most influential musical attributes for their genre in terms of popularity. 
+            They can focus on incorporating the features that positively impact their songs' popularity while avoiding those that detract 
+            from it, as indicated by the dataset.</p>
+        </div>
+      </div>
     </div>
   );
 }
