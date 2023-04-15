f4d79aaa6bb5e0797ae31d0dfd434211cad2e38b
Bryce <brycepcayanan@gmail.com>
2023-04-15 06:31:48 -0600

    Commit

--- src/Homepage.jsx
+++ src/Homepage.jsx
@@ -4,8 +4,9 @@ import "./Homepage.css"
 function Homepage() {
 
   return (
-    <div style={{height: "100vh", width: "100%", backgroundColor: "blue"}}>
-
+    <div className="homepage-container">
+      <h1 style={{fontSize: "4rem"}}>Have you ever wondered what makes a song...</h1>
+      <h1 style={{fontSize: "10rem"}}>POPULAR?</h1>
     </div>
   );
 }
